fb569da61d2a2efb12bfdf0466cd0ea76ed4adda
milasno@ua.fm <999999999_z>
2023-02-15 22:43:22 +0200

    selectArr

--- src/components/constructor/ConstructorOptions.jsx
+++ src/components/constructor/ConstructorOptions.jsx
@@ -1,31 +1,31 @@
 import styled from 'styled-components';
-import { Dropdown, Menu } from 'semantic-ui-react';
-import _ from 'lodash'
+
 
 import { colors, pageSize } from '../../styles.js';
+import { useState } from 'react';
 
 const { firstTextColor, secondTextColor, secondBackgroundColor, thirdTextColor } = colors;
 
 const ConstructorOptions = (props) => {
+
+  let allTags = ['Header', 'main', 'left_aside', 'right_aside', 'footer', 'div', 'p', 'ul', 'li', 'ol', 'form']
+
+  allTags.forEach(itm => {
+    
+  })
+
+  const [tags, setTags] = useState([]);
   
-  const getOptions = (number, prefix = 'Choice ') =>
-  _.times(number, (index) => ({
-    key: index,
-    text: `${prefix}${index}`,
-    value: index,
-  }))
   return (
     <OptionsWrapper>
       <h2>Options</h2>
-        <Menu vertical>
-        <Menu.Item as='a'>Link 1</Menu.Item>
-        <Menu.Item as='a'>Link 2</Menu.Item>
-        <Menu.Item header>All Sections</Menu.Item>
-        <Menu.Item>
-        <Dropdown placeholder='More' fluid selection options={getOptions(8)} />
-        </Menu.Item>
-      </Menu>
-      
+
+      <select id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+      </select>   
     </OptionsWrapper>
   );
 };
