63032116250ecb130040fb12b0a0c2d8d75fad3d
milasno@ua.fm <999999999_z>
2023-02-15 22:17:34 +0200

    add_dropdown

--- src/components/constructor/ConstructorOptions.jsx
+++ src/components/constructor/ConstructorOptions.jsx
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
+import { Dropdown, Menu } from 'semantic-ui-react';
+import _ from 'lodash'
 
 import { colors, pageSize } from '../../styles.js';
 
 const { firstTextColor, secondTextColor, secondBackgroundColor, thirdTextColor } = colors;
 
 const ConstructorOptions = (props) => {
+  
+  const getOptions = (number, prefix = 'Choice ') =>
+  _.times(number, (index) => ({
+    key: index,
+    text: `${prefix}${index}`,
+    value: index,
+  }))
   return (
     <OptionsWrapper>
       <h2>Options</h2>
+        <Menu vertical>
+        <Menu.Item as='a'>Link 1</Menu.Item>
+        <Menu.Item as='a'>Link 2</Menu.Item>
+        <Menu.Item header>All Sections</Menu.Item>
+        <Menu.Item>
+        <Dropdown placeholder='More' fluid selection options={getOptions(8)} />
+        </Menu.Item>
+      </Menu>
+      
     </OptionsWrapper>
   );
 };
